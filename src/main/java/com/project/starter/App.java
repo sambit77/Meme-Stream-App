2b5f4fecb2d13d7ae15968747e5d7f981d536347
Sambit Pradhan <sambitgulu@gmail.com>
2024-03-25 04:28:59 +0530

    sync up

--- src/main/java/com/project/starter/App.java
+++ src/main/java/com/project/starter/App.java
@@ -14,7 +14,7 @@ public class App {
 
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
-    System.out.println("Hello World");
+    System.out.println("Bye");
     //ArrayList<Integer> al = new ArrayList<>();
     
   }
